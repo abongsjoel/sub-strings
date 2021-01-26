bb6bde5dad257936c59fb1dbed2134e586cd0e14
tj-ubuntu <abongsjoel@gmail.com>
2021-01-26 07:48:01 +0100

    Make function case-insensitive

--- main.rb
+++ main.rb
@@ -2,17 +2,17 @@ def substrings(str, dictionary)
   new_dictionary = sub_dictionary(str, dictionary)
   
   new_dictionary.reduce(Hash.new(0)) do |result, word|
-    result[word] += 1
+    result[word.downcase] += 1
     result
   end
 end
 
 def sub_dictionary(str, dictionary)
   dictionary.filter do |word|
-    str.include?(word)
+    str.include?(word.downcase)
   end
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit","low"]
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit","Low"]
  
  p substrings("below", dictionary)
